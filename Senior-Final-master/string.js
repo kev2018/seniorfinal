606d991a3affece40b9ddd93d80a2f6723cc5eef
kev2018 <8kludwig@stpatrick.org>
2017-05-01 13:55:45 -0500

    Add files via upload

---
+++ Senior-Final-master/string.js
@@ -0,0 +1,56 @@
+var exports = module.exports = {};
+
+function PasswordMessage(m){
+this.name = "Password Message";
+this.message = m;
+}
+
+function inRange(char,min,max){
+    let unicode = char.charCodeAt(0);
+    if(char<min) || (char>max){
+      return false;
+    }
+    else {
+      return true;
+    }
+}
+
+exports.checkLength = function(str){
+    let valid = (str.length >= 8 && str.length <= 20);
+    try{
+        if(str.length < 8){
+            throw new PasswordMessage("Your password is too short. Should be at least 8 letters.");
+        }
+        else if(str.length > 20){
+            throw new PasswordMessage("Your password is too long. Should be at most 20 letters.");
+        }
+        else{
+            throw new PasswordMessage("Your password is an appropriate length.");
+        }
+    }
+    catch(e){
+        console.log(e.name+": "+e.message);
+        return valid;
+    }
+}
+
+
+exports.containsUpper =function(str){
+  let hasUpper = false;
+
+}
+
+
+exports.containsLower =function(str){
+
+}
+
+
+exports.containsNumerical =function(str){
+
+}
+
+
+exports.containsSpecial =function(str){
+
+}
